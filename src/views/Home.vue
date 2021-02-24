7b4457f916148d86cbdbdd4ff4a1df88b56f0032
SamRong <YungSun3307@163.com>
2021-02-24 22:59:02 +0800

    完成首页制作

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,6 +13,31 @@
       <a href="#/doc"> Start </a>
     </div>
   </div>
+  <footer>
+    <ul>
+      <li>
+        <svg class="icon">
+          <use xlink:href="#icon-vue"></use>
+        </svg>
+        <h3>基于 Vue 3</h3>
+        <p>骄傲地使用了Vue 3 Composition API</p>
+      </li>
+      <li>
+        <svg class="icon">
+          <use xlink:href="#icon-ts"></use>
+        </svg>
+        <h3>基于 TypeScript</h3>
+        <p>源码采用 TypeScript 编写</p>
+      </li>
+      <li>
+        <svg class="icon">
+          <use xlink:href="#icon-easy"></use>
+        </svg>
+        <h3>代码易读</h3>
+        <p>各组件源码极其简洁</p>
+      </li>
+    </ul>
+  </footer>
 </template>
 
 <script lang="ts">
@@ -59,6 +84,55 @@ export default {
     > a:hover {
       color: rgba(245, 166, 35);
       border: 2px solid rgba(51, 51, 51);
+    }
+  }
+}
+
+footer {
+  margin: 0 auto;
+  width: 1100px;
+  margin-top: 30px;
+  @media (max-width: 1100px) {
+    width: 700px;
+    margin-top: 0;
+  }
+  @media (max-width: 700px) {
+    width: 380px;
+    margin-top: 0;
+  }
+  > ul {
+    padding: 0;
+    margin: 0;
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    > li {
+      list-style: none;
+      display: grid;
+      grid-template-areas:
+        "icon title"
+        "icon text";
+      grid-template-columns: 80px auto;
+      grid-template-rows: 1fr auto;
+      justify-content: start;
+      margin-bottom: 40px;
+      .icon {
+        width: 64px;
+        height: 64px;
+        fill: #333;
+        overflow: hidden;
+        grid-area: icon;
+      }
+      h3 {
+        margin: 0;
+        font-size: 28px;
+        grid-area: title;
+        font-weight: normal;
+      }
+      p {
+        margin: 0;
+        grid-area: text;
+      }
     }
   }
 }
