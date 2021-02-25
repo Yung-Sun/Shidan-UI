74785e540c57d8c77459bb2a6c48b730a84fbe2f
SamRong <YungSun3307@163.com>
2021-02-25 22:09:35 +0800

    优化介绍页代码

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import "./lib/shidan.scss"
 import { createApp } from 'vue'
 import App from './App.vue'
-
+import Markdown from "./components/Markdown.vue"
 import { router } from './router'
 import 'github-markdown-css'
 const app = createApp(App)
 
 app.use(router)
 app.mount('#app')
+app.component("Markdown", Markdown)
 
 
--- src/router.ts
+++ src/router.ts
@@ -1,31 +1,33 @@
-import {createWebHashHistory, createRouter} from 'vue-router'
+import { createWebHashHistory, createRouter } from 'vue-router'
 import Home from './Views/Home.vue'
 import Doc from './Views/Doc.vue'
 import SwitchDemo from './components/SwitchDemo.vue'
 import ButtonDemo from './components/ButtonDemo.vue'
 import DialogDemo from './components/DialogDemo.vue'
 import TabsDemo from './components/TabsDemo.vue'
-import Introduce from './components/Introduce.vue'
-import Install from './components/Install.vue'
-import Start from './components/Start.vue'
+import Markdown from './components/Markdown.vue'
+import { h } from 'vue'
 
 
 const history = createWebHashHistory()
+const md = (fileName: string) => h(Markdown, { path: `../markdown/${fileName}.md`, key: fileName })
 export const router = createRouter({
     history: history,
     routes: [
-        {path:'/', component: Home},
-        {path:'/doc', component: Doc, children:[
-            {path:'introduce', component: Introduce},
-            {path:'install', component: Install},
-            {path:'start', component: Start},
-            {path:'switch', component: SwitchDemo},
-            {path:'button', component: ButtonDemo},
-            {path:'dialog', component: DialogDemo},
-            {path:'tabs', component: TabsDemo},
-        ]}
+        { path: '/', component: Home },
+        {
+            path: '/doc', component: Doc, children: [
+                { path: 'introduce', component: md("Introduce") },
+                { path: 'install', component: md("Install") },
+                { path: 'start', component: md("Start") },
+                { path: 'switch', component: SwitchDemo },
+                { path: 'button', component: ButtonDemo },
+                { path: 'dialog', component: DialogDemo },
+                { path: 'tabs', component: TabsDemo },
+            ]
+        }
     ],
 })
-router.afterEach(()=>{
-    
+router.afterEach(() => {
+
 })
