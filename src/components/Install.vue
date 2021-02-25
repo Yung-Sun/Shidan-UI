1abafc5a81c2cd34768b455a8ed8905687c7065a
SamRong <YungSun3307@163.com>
2021-02-25 19:37:40 +0800

    填充文档内容

--- src/components/Install.vue
+++ src/components/Install.vue
@@ -1,3 +1,13 @@
 <template>
-  <div>安装页</div>
+  <h1>安装</h1>
+  <hr />
+  <p>打开终端运行以下命令：</p>
+  <pre><code>npm install shidan-ui</code></pre>
+  <p>或</p>
+  <pre><code>yarn add shidan-ui</code></pre>
+
+  <p>
+    下一节：
+    <a href="#/doc/start">开始使用</a>
+  </p>
 </template>
--- src/components/Start.vue
+++ src/components/Start.vue
@@ -1,3 +1,13 @@
 <template>
-  <div>开始页</div>
+  <h1>开始页</h1>
+  <hr />
+  <p>请先<a href="#/doc/install">安装</a>本组件库。</p>
+  <p>然后在你的代码中写入下面的代码</p>
+  <pre><code>import {Button, Tabs, Tab, Switch, Dialog, openDialog} from "shidan-ui"
+</code></pre>
+  <p>就可以使用示蛋UI的组件了。</p>
+  <h2>Vue 单文件组件</h2>
+  <hr />
+  <p>代码示例：</p>
+  <pre><code></code></pre>
 </template>
